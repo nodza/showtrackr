7576c00a2c4145ecd780264e8c1192d77208442f
Noel Hwande <noel.hwande@gmail.com>
2014-12-03 19:15:53 -0600

    completed Step 11: Email Notifications

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ var _ = require('lodash');
 var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var agenda = require('agenda')({ db: { address: 'localhost:27017/test' } });
+var sugar = require('sugar');
+var nodemailer = require('nodemailer');
 
 
 // SHOW SCHEMA
@@ -206,6 +209,10 @@ app.post('/api/shows', function(req, res, next) {
                 }
                 return next(err);
             }
+
+            var alertDate = Date.create('Next ' + show.airsDayOfWeek + ' at ' + show.airsTime).rewind({ hour: 2});
+            agenda.schedule(alertDate, 'send email alert', show.name).repeatEvery('1 week');
+
             res.send(200);
         });
     });
@@ -283,6 +290,47 @@ app.post('/api/unsubscribe', ensureAuthenticated, function(req, res, next) {
     });
 });
 
+// MAILER CODE
+agenda.define('send email alert', function(job, done) {
+    Show.findOne({ name: job.attrs.data }).populate('subscribers').exec(function(err, show) {
+        var emails = show.subscribers.map(function(user) {
+            return user.email;
+        });
+
+        var upcomingEpisode = show.episodes.filter(function(episode) {
+            return new Date(episode.firstAired) > new Date();
+        })[0];
+
+        var smtpTransport = nodemailer.createTransport('SMTP', {
+            service: 'SendGrid',
+            auth: { user: 'hslogin', pass: 'hspassword00' }
+        });
+
+        var mailOptions = {
+            from: 'Fred Foo ✔ <foo@blurdybloop.com>',
+            to: emails.join(','),
+            subject: show.name + ' is starting soon!',
+            text: show.name + ' starts in less than 2 hours on ' + show.network + '.\n\n' +
+            'Episode ' + upcomingEpisode.episodeNumber + ' Overview\n\n' + upcomingEpisode.overview
+        };
+
+        smtpTransport.sendMail(mailOptions, function(error, response) {
+            console.log('Message sent: ' + response.message);
+            smtpTransport.close();
+            done();
+        });
+    });
+});
+
+agenda.start();
+
+agenda.on('start', function(job) {
+    console.log("Job %s starting", job.attrs.name);
+});
+
+agenda.on('complete', function(job) {
+    console.log("Job %s finished", job.attrs.name);
+});
 
 function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) next();
